9ba74a9c774847a639637d2ad79bede41c68279b
Nikolaipe <nikolaipe@getacademy.no>
2023-03-07 17:57:24 +0100

    Prøve å kjøre import i viewet
    le shrug

--- view.js
+++ view.js
@@ -1,3 +1,8 @@
-function updateView(){
-    main.innerHTML = "testing 07.03 16:48, 17:10";
+function updateView() {
+    import("/firebase.js")
+    .then((module) => {
+      window.testRead = module.testRead;
+    })
+    .catch((error) => console.error(error));
+  main.innerHTML = `testing 07.03 16:48, 17:10, 17:56 <button onclick="testRead()">test</button>`;
 }
